0a07152c15991e816fafa0189ff524b7e449c7e1
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-10-08 06:05:02 +0900

    페이지 생성

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router";
 function MainPage() {
     return (
         <div>
-            <div><NavLink to='/about'>About</NavLink></div>
-            <div>Main Page</div>
-        </div>
+            어서오세요 <br /> 지금부터 음악을 만들어볼까요 ? 👽
+            <div><NavLink to='/what1'>허밍 올리는 버튼</NavLink></div>
+        </div >
     );
 }
 
--- src/router/root.tsx
+++ src/router/root.tsx
@@ -5,6 +5,9 @@ const Loading = () => <div>Loading.........</div>
 const Main = lazy(() => import("../pages/mainPage"))
 const About = lazy(() => import("../pages/aboutPage"))
 const Soohyun = lazy(() => import("../pages/soohyunPage"))
+const What1 = lazy(() => import("../pages/whatPage1.tsx"))
+const What2 = lazy(() => import("../pages/whatPage2.tsx"))
+const MusicResult = lazy(() => import("../pages/musicResultPage.tsx"))
 
 const router = createBrowserRouter([
     {
@@ -18,6 +21,18 @@ const router = createBrowserRouter([
     {
         path: "soohyun",
         element: <Suspense fallback={<Loading />}><Soohyun /></Suspense>
+    },
+    {
+        path: "what1",
+        element: <Suspense fallback={<Loading />}><What1 /></Suspense>
+    },
+    {
+        path: "what2",
+        element: <Suspense fallback={<Loading />}><What2 /></Suspense>
+    },
+    {
+        path: "musicResult",
+        element: <Suspense fallback={<Loading />}><MusicResult /></Suspense>
     }
 ]);
 

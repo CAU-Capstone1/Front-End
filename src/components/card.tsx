c5cb0e46e77189e0d97b4d6ba9971506bf562bd8
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-10-08 17:00:01 +0900

    화면 크기 조정

---
+++ src/components/card.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react";
+
+interface CardProps {
+    children: ReactNode;
+}
+
+function Card({ children }: CardProps) {
+    return ( 
+        
+     );
+}
+
+export default Card;

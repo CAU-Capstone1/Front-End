64812d744c55f022a87f08d2c8677e6aefea7013
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-12-04 15:55:25 +0900

    로그인시 startpage 로 이동

--- src/pages/M_loginPage.tsx
+++ src/pages/M_loginPage.tsx
@@ -30,7 +30,7 @@ function LoginPage() {
             }
             
             alert("로그인 성공!");
-            navigate("/myPage");
+            navigate("/");
         } catch (err) {
             setError(err instanceof Error ? err.message : "로그인에 실패했습니다.");
         } finally {

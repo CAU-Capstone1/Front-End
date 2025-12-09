2a09159571933b866b7c1edf0e199005373ffb54
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-12-09 16:45:05 +0900

    화면전환 애니메이션

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,12 +1,21 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import GlobalHeader from "../components/GlobalHeader";
 import CursorSparkles from "../components/CursorSparkles";
 
 export default function AppLayout() {
+    const location = useLocation();
+
     return (
         <>
             <GlobalHeader />
-            <Outlet />
+            <div className="page-transition-wrapper">
+                <div 
+                    key={location.pathname} 
+                    className="page-content fade-in-up"
+                >
+                    <Outlet />
+                </div>
+            </div>
             <CursorSparkles />
         </>
     );

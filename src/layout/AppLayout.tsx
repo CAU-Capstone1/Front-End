7a3d37f048a724bdc3049ab0bbe904f29af42635
Y00nS00Hyun <yoonsooh1114@gmail.com>
2025-12-09 15:23:48 +0900

    커서에 반짝이 추가

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import GlobalHeader from "../components/GlobalHeader";
+import CursorSparkles from "../components/CursorSparkles";
 
 export default function AppLayout() {
     return (
         <>
             <GlobalHeader />
             <Outlet />
+            <CursorSparkles />
         </>
     );
 }
